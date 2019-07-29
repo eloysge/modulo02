38455430119ba97634b28b4f62106fd3cf6b202d
Eloy Gontijo Martins <eloy@sgeinformatica.com.br>
2019-07-29 17:52:25 -0300

    modulo02: mongodb

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import Mongoose from 'mongoose';
 import User from '../app/models/User';
 import File from '../app/models/File';
 import Appointment from '../app/models/Appointment';
@@ -9,13 +10,21 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
-  init() {
-    this.connection = new Sequelize(databaseConfig);
+  async init() {
+    this.connection = await new Sequelize(databaseConfig);
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  async mongo() {
+    this.mongoConnection = await Mongoose.connect(
+      'mongodb://192.168.0.78:27017/gobarber',
+      { useNewUrlParser: true, useFindAndModify: true }
+    );
   }
 }
 

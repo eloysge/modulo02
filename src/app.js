95b74970bea8c4cea340253be8112ef271d8953c
Eloy Gontijo Martins <eloy@sgeinformatica.com.br>
2019-09-18 10:12:22 -0300

    modulo02: ajustes para modulo09 (gobarber web)

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import 'express-async-errors';
 import * as Sentry from '@sentry/node';
 import Youch from 'youch';
 import path from 'path';
+import cors from 'cors';
 import sentryConfig from './config/sentry';
 import routes from './routes';
 
@@ -22,6 +23,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 import authConfig from '../../config/auth';
 
 class SessionController {
@@ -17,7 +18,16 @@ class SessionController {
     }
 
     const { email, password } = req.body;
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     if (!user) {
       return res.status(401).json({ error: 'Usuário não cadastrado' });
@@ -27,13 +37,15 @@ class SessionController {
       return res.status(401).json({ error: 'Senha não confere' });
     }
 
-    const { id, name } = user;
+    const { id, name, avatar, provider } = user;
 
     return res.json({
       user: {
         id,
         name,
         email,
+        provider,
+        avatar,
         timeLive: authConfig.expiresIn,
       },
       token: jwt.sign({ id }, authConfig.secret, {

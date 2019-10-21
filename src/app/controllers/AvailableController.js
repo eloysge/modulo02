a71aee557e6e4c18855efd7c7ff5159c1686f221
Eloy Gontijo Martins <eloy@sgeinformatica.com.br>
2019-10-21 15:27:56 -0300

    modulo02: timezone:strings

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -82,7 +82,10 @@ class AvailableController {
         appointment => format(appointment.date, 'HH:mm') === time
       );
 
-      const compareDate = utcToZonedTime(new Date(), 'America/Sao_Paulo');
+      const compareDate = utcToZonedTime(
+        new Date(),
+        `${process.env.TIME_ZONE}`
+      );
 
       return {
         time,
